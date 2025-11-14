b3b4bf18019c016a2d21b7575c32699d1b579798
CranberrySoup <142951702+CranberrySoup@users.noreply.github.com>
2025-11-14 20:48:10 +0000

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lagradost.aniyomicompat"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "2.0.0"
+        versionCode = 11
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
